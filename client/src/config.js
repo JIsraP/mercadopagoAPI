210a4a206ad16ee5cb8a1f5aa154e58313c5e381
JIsraP <a01423294@tec.mx>
2024-06-30 18:39:47 -0600

    .env variables

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,7 @@
 export const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
-export const apiKey = import.meta.env.VITE_API_KEY;
+export const FB_API_KEY = import.meta.env.VITE_API_KEY;
+export const FB_AUTH_DOMAIN = import.meta.env.VITE_AUTH_DOMAIN;
+export const FB_PROJECT_ID = import.meta.env.VITE_PROJECT_ID;
+export const FB_STORAGE_BUCKET = import.meta.env.VITE_STORAGE_BUCKET;
+export const FB_MESSAGING_SENDER_ID = import.meta.env.VITE_MESSAGING_SENDER_ID;
+export const FB_APP_ID = import.meta.env.VITE_APP_ID;
